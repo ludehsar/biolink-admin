ea2670ad277d976dfa768d2c99158baf42167fcf
Rashedul Alam <rashedulalam112112@gmail.com>
2021-05-22 23:05:58 +0600

    Initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,8 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import React from 'react'
+import { NextPage } from 'next'
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
+const IndexPage: NextPage = () => (
+  <div>Hello</div>
 )
 
 export default IndexPage
