7c9e97bd8617df86e34e688407abee6abeb57518
Rashedul Alam <rashedulalam112112@gmail.com>
2021-06-28 22:47:13 +0600

    Implemented showing lists of verifications

---
+++ src/pages/verifications/rejected.tsx
@@ -0,0 +1,146 @@
+import React, { useCallback, useState } from 'react'
+import { NextPage } from 'next'
+import { withUrqlClient } from 'next-urql'
+import { UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap'
+import Link from 'next/link'
+import moment from 'moment'
+
+import DataTable from '../../components/DataTable/DataTable'
+import AdminHeader from '../../components/Header/AdminHeader'
+import { useGetAllRejectedVerificationsQuery } from '../../generated/graphql'
+import AdminLayout from '../../layouts/Admin.layout'
+import { createUrqlClient } from '../../utils/createUrqlClient'
+
+const columns = [
+  {
+    name: 'Username',
+    selector: 'username',
+  },
+  {
+    name: 'Category',
+    selector: 'category',
+  },
+  {
+    name: 'First Name',
+    selector: 'firstName',
+  },
+  {
+    name: 'Last Name',
+    selector: 'lastName',
+  },
+  {
+    name: 'Mobile Number',
+    selector: 'mobileNumber',
+  },
+  {
+    name: 'Work Number',
+    selector: 'workNumber',
+  },
+  {
+    name: 'Email',
+    selector: 'email',
+  },
+  {
+    name: 'Website Link',
+    selector: 'websiteLink',
+  },
+  {
+    name: 'User',
+    selector: 'user',
+  },
+  {
+    name: 'Biolink',
+    selector: 'biolink',
+  },
+  {
+    name: 'Created',
+    selector: 'createdAt',
+  },
+  {
+    name: '',
+    selector: 'action',
+  },
+]
+
+const RejectedVerificationsIndexPage: NextPage = () => {
+  const [after, setAfter] = useState<string>('')
+  const [before, setBefore] = useState<string>('')
+  const [searchText, setSearchText] = useState<string>('')
+  const [{ data }] = useGetAllRejectedVerificationsQuery({
+    variables: { options: { first: 10, after, before, query: searchText } },
+  })
+
+  const gotoPrevPage = useCallback(() => {
+    setBefore(data?.getAllRejectedVerifications?.pageInfo?.startCursor || '')
+    setAfter('')
+  }, [data?.getAllRejectedVerifications?.pageInfo?.startCursor])
+
+  const gotoNextPage = useCallback(() => {
+    setAfter(data?.getAllRejectedVerifications?.pageInfo?.endCursor || '')
+    setBefore('')
+  }, [data?.getAllRejectedVerifications?.pageInfo?.endCursor])
+
+  const userData =
+    data?.getAllRejectedVerifications?.edges?.map((edge) => ({
+      username: edge.node.username,
+      category: edge.node.category?.categoryName,
+      firstName: edge.node.firstName,
+      lastName: edge.node.lastName,
+      mobileNumber: edge.node.mobileNumber,
+      workNumber: edge.node.workNumber,
+      email: edge.node.email,
+      websiteLink: edge.node.websiteLink,
+      user: (
+        <Link href={'/users/view/' + edge.node.user?.id}>
+          <a href={'/users/view/' + edge.node.user?.id}>{edge.node.user?.email}</a>
+        </Link>
+      ),
+      biolink: (
+        <Link href={'/biolinks/view/' + edge.node.biolink?.id}>
+          <a href={'/biolinks/view/' + edge.node.biolink?.id}>
+            {edge.node.biolink?.username?.username}
+          </a>
+        </Link>
+      ),
+      createdAt: moment.unix(parseInt(edge.node.createdAt || '') / 1000).format('DD-MM-YYYY'),
+      action: (
+        <UncontrolledDropdown>
+          <DropdownToggle
+            className="btn-icon-only text-light"
+            href="#pablo"
+            role="button"
+            size="sm"
+            color=""
+            onClick={(e) => e.preventDefault()}
+          >
+            <i className="fas fa-ellipsis-v" />
+          </DropdownToggle>
+          <DropdownMenu className="dropdown-menu-arrow" right>
+            <Link href={'/categories/edit/' + edge.node.id}>
+              <DropdownItem href={'/categories/edit/' + edge.node.id}>Edit</DropdownItem>
+            </Link>
+          </DropdownMenu>
+        </UncontrolledDropdown>
+      ),
+    })) || []
+
+  return (
+    <AdminLayout>
+      <AdminHeader />
+      <DataTable
+        title="Rejected Verifications"
+        newButton={true}
+        newButtonLink="/categories/add"
+        columns={columns}
+        data={userData}
+        hasNextPage={data?.getAllRejectedVerifications?.pageInfo?.hasNextPage || false}
+        hasPreviousPage={data?.getAllRejectedVerifications?.pageInfo?.hasPreviousPage || false}
+        nextButtonAction={gotoNextPage}
+        prevButtonAction={gotoPrevPage}
+        setSearchText={(text) => setSearchText(text)}
+      />
+    </AdminLayout>
+  )
+}
+
+export default withUrqlClient(createUrqlClient)(RejectedVerificationsIndexPage)
---
+++ src/pages/verifications/verified.tsx
@@ -0,0 +1,146 @@
+import React, { useCallback, useState } from 'react'
+import { NextPage } from 'next'
+import { withUrqlClient } from 'next-urql'
+import { UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap'
+import Link from 'next/link'
+import moment from 'moment'
+
+import DataTable from '../../components/DataTable/DataTable'
+import AdminHeader from '../../components/Header/AdminHeader'
+import { useGetAllVerifiedVerificationsQuery } from '../../generated/graphql'
+import AdminLayout from '../../layouts/Admin.layout'
+import { createUrqlClient } from '../../utils/createUrqlClient'
+
+const columns = [
+  {
+    name: 'Username',
+    selector: 'username',
+  },
+  {
+    name: 'Category',
+    selector: 'category',
+  },
+  {
+    name: 'First Name',
+    selector: 'firstName',
+  },
+  {
+    name: 'Last Name',
+    selector: 'lastName',
+  },
+  {
+    name: 'Mobile Number',
+    selector: 'mobileNumber',
+  },
+  {
+    name: 'Work Number',
+    selector: 'workNumber',
+  },
+  {
+    name: 'Email',
+    selector: 'email',
+  },
+  {
+    name: 'Website Link',
+    selector: 'websiteLink',
+  },
+  {
+    name: 'User',
+    selector: 'user',
+  },
+  {
+    name: 'Biolink',
+    selector: 'biolink',
+  },
+  {
+    name: 'Created',
+    selector: 'createdAt',
+  },
+  {
+    name: '',
+    selector: 'action',
+  },
+]
+
+const VerifiedVerificationsIndexPage: NextPage = () => {
+  const [after, setAfter] = useState<string>('')
+  const [before, setBefore] = useState<string>('')
+  const [searchText, setSearchText] = useState<string>('')
+  const [{ data }] = useGetAllVerifiedVerificationsQuery({
+    variables: { options: { first: 10, after, before, query: searchText } },
+  })
+
+  const gotoPrevPage = useCallback(() => {
+    setBefore(data?.getAllVerifiedVerifications?.pageInfo?.startCursor || '')
+    setAfter('')
+  }, [data?.getAllVerifiedVerifications?.pageInfo?.startCursor])
+
+  const gotoNextPage = useCallback(() => {
+    setAfter(data?.getAllVerifiedVerifications?.pageInfo?.endCursor || '')
+    setBefore('')
+  }, [data?.getAllVerifiedVerifications?.pageInfo?.endCursor])
+
+  const userData =
+    data?.getAllVerifiedVerifications?.edges?.map((edge) => ({
+      username: edge.node.username,
+      category: edge.node.category?.categoryName,
+      firstName: edge.node.firstName,
+      lastName: edge.node.lastName,
+      mobileNumber: edge.node.mobileNumber,
+      workNumber: edge.node.workNumber,
+      email: edge.node.email,
+      websiteLink: edge.node.websiteLink,
+      user: (
+        <Link href={'/users/view/' + edge.node.user?.id}>
+          <a href={'/users/view/' + edge.node.user?.id}>{edge.node.user?.email}</a>
+        </Link>
+      ),
+      biolink: (
+        <Link href={'/biolinks/view/' + edge.node.biolink?.id}>
+          <a href={'/biolinks/view/' + edge.node.biolink?.id}>
+            {edge.node.biolink?.username?.username}
+          </a>
+        </Link>
+      ),
+      createdAt: moment.unix(parseInt(edge.node.createdAt || '') / 1000).format('DD-MM-YYYY'),
+      action: (
+        <UncontrolledDropdown>
+          <DropdownToggle
+            className="btn-icon-only text-light"
+            href="#pablo"
+            role="button"
+            size="sm"
+            color=""
+            onClick={(e) => e.preventDefault()}
+          >
+            <i className="fas fa-ellipsis-v" />
+          </DropdownToggle>
+          <DropdownMenu className="dropdown-menu-arrow" right>
+            <Link href={'/categories/edit/' + edge.node.id}>
+              <DropdownItem href={'/categories/edit/' + edge.node.id}>Edit</DropdownItem>
+            </Link>
+          </DropdownMenu>
+        </UncontrolledDropdown>
+      ),
+    })) || []
+
+  return (
+    <AdminLayout>
+      <AdminHeader />
+      <DataTable
+        title="Verified Verifications"
+        newButton={true}
+        newButtonLink="/categories/add"
+        columns={columns}
+        data={userData}
+        hasNextPage={data?.getAllVerifiedVerifications?.pageInfo?.hasNextPage || false}
+        hasPreviousPage={data?.getAllVerifiedVerifications?.pageInfo?.hasPreviousPage || false}
+        nextButtonAction={gotoNextPage}
+        prevButtonAction={gotoPrevPage}
+        setSearchText={(text) => setSearchText(text)}
+      />
+    </AdminLayout>
+  )
+}
+
+export default withUrqlClient(createUrqlClient)(VerifiedVerificationsIndexPage)
